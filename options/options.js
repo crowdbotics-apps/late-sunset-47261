fe666460fe2cb7cc96705fcc651a4e7e43c2bf79
Crowdbotics <team@crowdbotics.com>
2024-03-25 14:40:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-sunset-47261",
+  url: "https://late-sunset-47261.botics.co",
+  api: "https://late-sunset-47261.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
